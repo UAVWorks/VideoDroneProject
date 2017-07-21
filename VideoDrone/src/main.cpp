4ebe271322b12ee873d21662fd2bf6ec2ba64059
Rakshit Allamraju <rakshitdayal.allamraju@gmail.com>
2017-07-21 00:04:53 -0500

    First commit

---
+++ VideoDrone/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "../include/globals/global.h"
+#include "../include/camera/VideoCamera.h"
+#include "../include/control/aclmccontroller.h"
+#include "../include/imu/BerryGPSIMU.h"
+
+int main(){
+
+    std::cout << "main" <<std::endl;
+    return 0;
+}
