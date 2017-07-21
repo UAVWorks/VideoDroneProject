6a0ea79c99554573431c922f66bdabc137cc2fc0
Rakshit Allamraju <rakshitdayal.allamraju@gmail.com>
2017-07-21 00:04:48 -0500

    Using eigen libraries

--- VideoDrone/src/control/aclmccontroller.cpp
+++ VideoDrone/src/control/aclmccontroller.cpp
@@ -15,84 +15,53 @@ ACLMCController::ACLMCController(){
     mass = 10;//TEMP ,load from config
     dT = 1.0/10.0;
 
-    r.set_size(3);
-    r.fill(0.0);
-    r_dot.set_size(3);
-    r_dot.fill(0.0);
-    r_ddot.set_size(3);
-    r_dot.fill(0.0);
-
-    r_fb.set_size(3);
-    r_fb.fill(0.0);
-
-    desiredr.set_size(3);
-    desiredr_dot.set_size(3);
-    desiredr_ddot.set_size(3);
-    desiredr.fill(0.0);
-    desiredr_dot.fill(0.0);
-    desiredr_ddot.fill(0.0);
-
-    desiredr_prev.set_size(3);
-    desiredr_dot_prev.set_size(3);
-    desiredr_ddot_prev.set_size(3);
-    desiredr_prev.fill(0.0);
-    desiredr_dot_prev.fill(0.0);
-    desiredr_ddot_prev.fill(0.0);
-
-    e.set_size(3);
-    e.fill(0.0);
-
-    e_dot.set_size(3);
-    e_dot.fill(0.0);
-
-    q_imu.R_component_1() = 0.0;
-    q_imu.R_component_2() = 0.0;
-    q_imu.R_component_3() = 0.0;
-    q_imu.R_component_4() = 0.0;
-
-    q_d.R_component_1() = 0.0;
-    q_d.R_component_2() = 0.0;
-    q_d.R_component_3() = 0.0;
-    q_d.R_component_4() = 0.0;
-
-    q_e.R_component_1() = 0.0;
-    q_e.R_component_2() = 0.0;
-    q_e.R_component_3() = 0.0;
-    q_e.R_component_4() = 0.0;
-
-    Fbar_b.set_size(3);
-    Fbar_b.fill(0.0);
-
-    Fbar_i.set_size(3);
-    Fbar_i.fill(0.0);
-
-    gravity_vec(0) = 0;
-    gravity_vec(1) = 0;
     gravity_vec(2) = 9.81;
 
 }
 
-void ACLMCController::loadGPSIMU(float GPS[3], float IMU[3]){
+void ACLMCController::setPosKp(Eigen::Matrix3f mat){
+    kp = mat;
+}
+
+void ACLMCController::setPosKd(Eigen::Matrix3f mat){
+    kd = mat;
+}
+
+void ACLMCController::setAttKp(Eigen::Matrix3f mat){
+    Kp = mat;
+}
+
+void ACLMCController::setAttKd(Eigen::Matrix3f mat){
+    Kd = mat;
+}
+
+void ACLMCController::loadGPSIMU(float GPS[3], float IMU[3], float W[3], home myHome){ // GPS,IMU and Ang Velocity
     // Convert IMU data to quaternion
     EulertoQuaternion(IMU[0], IMU[1], IMU[2], q_imu);
+    // get NED data
     gps2ned(GPS, myHome, r);
-
+    Omega(0) = W[0];
+    Omega(1) = W[1];
+    Omega(2) = W[2];
 }
 
 void ACLMCController::loadDesired(float desired_loc[3], float desired_psi){
-    desiredr = desired_loc;
+    desiredr(0) = desired_loc[0];
+    desiredr(1) = desired_loc[1];
+    desiredr(2) = desired_loc[2];
+
     psi_d = desired_psi;
 }
 
 void ACLMCController::GenerateControl(){
 
-    arma::colvec F_i(3), F_b(3);
-    arma::colvec Fb_test(3);
-    Fb_test.fill(0.0);
-    F_i.fill(0.0);
-    F_b.fill(0.0);
-
-    boost::math::quaternion<float> q_psi_convert(cos(psi_d/2),0,0,sin(psi_d/2));
+    Eigen::Vector3f F_i, F_b;
+    Eigen::Vector3f Fb_test;
+    Eigen::Vector3f Fbardot_i, Omega_d;
+    float psidot_d;
+    Eigen::Quaternionf Q_psid;
+    Q_psid.w() = cos(psi_d/2);
+    Q_psid.vec()(2) = sin(psi_d/2);
 
     desiredr_dot = (desiredr-desiredr_prev)*(1/dT);
     desiredr_prev = desiredr;
@@ -100,36 +69,47 @@ void ACLMCController::GenerateControl(){
     desiredr_ddot = (desiredr_dot - desiredr_dot_prev)*(1/dT);
     desiredr_dot_prev = desiredr_dot;
 
+    desiredr_dddot = (desiredr_ddot - desiredr_ddot_prev)*(1/dT);
+    desiredr_ddot_prev = desiredr_ddot;
+
     e = r - desiredr;
     e_dot = (r_dot - desiredr_dot)*(1/dT);
 
-    r_fb = -dot(Kp,e) - dot(Kd,e_dot); //PD for now
+    r_fb = -kp*e - kd*e_dot; //PD for now
 
     F_i = (mass*(desiredr_ddot + r_fb + gravity_vec));
 
+    Fdot_i = (F_i - F_i_prev)*(1/dT);
+    F_i_prev = F_i;
+
     Fb_test(2) = 1;
-    if( dot(F_i,Fb_test) >= 0){
+    if( F_i.dot(Fb_test) >= 0){
         F_b(2) = 1;
     }else{
         F_b(2) = -1;
     }
 
-    Fbar_i = F_i/norm(F_i,2);
-    Fbar_b = F_b/norm(F_b,2);
+    Fbar_i = F_i/F_i.norm();
+    Fbar_b = F_b/F_b.norm();
 
-    f_total = norm(F_i,2);
+    f_total = F_i.norm();
 
-    q_d.R_component_1() = 1+ dot(Fbar_i,Fbar_b);
-    q_d.R_component_2() = cross(Fbar_i, Fbar_b)(0);
-    q_d.R_component_3() = cross(Fbar_i, Fbar_b)(1);
-    q_d.R_component_4() = cross(Fbar_i, Fbar_b)(2);
+    q_d.w() = (1 + Fbar_i.dot(Fbar_b))*(1/sqrt(2*(1+Fbar_i.dot(Fbar_b))));
+    q_d.vec() = Fbar_i.cross(Fbar_b)*(1/sqrt(2*(1+Fbar_i.dot(Fbar_b))));
 
-    q_d *= 1.0/sqrt(2*(1+dot(Fbar_i,Fbar_b)));
+    q_d = q_d*Q_psid;
 
-    q_d *= q_psi_convert; // desired attitude
+    Fbardot_i = (Fdot_i/F_i.norm()) - (F_i*(F_i.dot(Fdot_i)))/pow(F_i.norm(),3);
 
+    q_e = q_imu.inverse()*q_d;
 
-    q_e = conj(q_imu)*q_d;
+    psidot_d = (psi_d - psi_d_prev)*(1/dT);
+    psi_d_prev = psi_d;
 
-    M_b = -sgn(q_e.R_component_1())*Kp*q_e(1,2,3);
-}
+    Omega_d(0) = Fbar_i.cross(Fbardot_i)(0);
+    Omega_d(1) = Fbar_i.cross(Fbardot_i)(1);
+    Omega_d(2) =psidot_d;
+
+    M_b = -sgn(q_e.w())*Kp*q_e.vec() - Kd*(Omega - Omega_d);
+
+    }
